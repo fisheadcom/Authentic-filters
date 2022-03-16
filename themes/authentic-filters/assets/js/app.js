5956293e074fb8c9ab19d4bc02366232918afea0
SNAPPERITOFFICE\anudeep.k <anudeep.k@snapperit.com>
2022-03-16 14:45:14 +0530

    footer and faq and small changes in header files

--- themes/authentic-filters/assets/js/app.js
+++ themes/authentic-filters/assets/js/app.js
@@ -1,23 +1,36 @@
-const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav-links");
-    const navLinks = document.querySelectorAll(".nav-links li");
+$(document).ready(function () {
 
-    burger.addEventListener("click", () => {
-        nav.classList.toggle("nav-active");
 
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = "";
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${
-                    index / 7 + 0.3
-                }s`;
-            }
+    const navSlide = () => {
+        const burger = document.querySelector(".burger");
+        const nav = document.querySelector(".nav-links");
+        const navLinks = document.querySelectorAll(".nav-links li");
+
+        burger.addEventListener("click", () => {
+            nav.classList.toggle("nav-active");
+
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = "";
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${
+                        index / 7 + 0.3
+                    }s`;
+                }
+            });
+
+            burger.classList.toggle("toggle");
         });
+    };
 
-        burger.classList.toggle("toggle");
+    navSlide();
+
+
+    $(".faq-heading").click(function () {
+        $(this)
+            .parent("li")
+            .toggleClass("the-active")
+            .find(".faq-text")
+            .slideToggle();
     });
-};
-
-navSlide();
+});
